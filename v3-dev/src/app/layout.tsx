5b99366bfdce6c0bf931daf459ef804eb18f87c1
chrisdten3 <123044283+chrisdten3@users.noreply.github.com>
2025-11-21 16:04:40 -0500

    Adding vercel analytics

--- v3-dev/src/app/layout.tsx
+++ v3-dev/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
