cd1a92f26fb26989ace46aadf0375eaaa1f0324c
chrisdten3 <123044283+chrisdten3@users.noreply.github.com>
2025-11-22 17:52:18 -0500

    Adding feedback button

--- v3-dev/src/app/layout.tsx
+++ v3-dev/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import FeedbackButton from "@/components/FeedbackButton";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}
       >
         {children}
+        <FeedbackButton />
         <Analytics />
       </body>
     </html>
